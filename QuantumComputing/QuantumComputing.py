47e176a9628219803dccda52f0aaf8fe83c8c6af
Daniel Sims <dsims@spearhead-capm.com>
2019-07-10 22:06:47 -0700

    Initial Commit

--- QuantumComputing/QuantumComputing.py
+++ QuantumComputing/QuantumComputing.py
@@ -1,1 +1,10 @@
+import dwavebinarycsp
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
 
+sampler = EmbeddingComposite(DWaveSampler())
+
+csp = dwavebinarycsp.factories.random_2in4sat(10, 7)
+bqm = dwavebinarycsp.stitch(csp)
+
+solution = sampler.sample(bqm)
